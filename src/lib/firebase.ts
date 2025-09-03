0ad4b353e2ccc58c9b6e08ac51b20310fca713d1
Bingdev <bingdev505@gmail.com>
2025-09-03 16:14:30 +0000

    there is not project named like that in could console

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  "projectId": "whatso-7wxaj",
-  "appId": "1:567542358936:web:48b098092b517a6ed241ed",
-  "storageBucket": "whatso-7wxaj.firebasestorage.app",
-  "apiKey": "AIzaSyAPyfM87tyo5ohpXhBWBwomL7yjCq7mI1A",
-  "authDomain": "whatso-7wxaj.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "567542358936"
+  apiKey: "PASTE_YOUR_API_KEY_HERE",
+  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
+  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
+  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "PASTE_YOUR_APP_ID_HERE",
+  measurementId: "PASTE_YOUR_MEASUREMENT_ID_HERE"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
